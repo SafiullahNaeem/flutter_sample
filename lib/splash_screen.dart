64139861f5f3af847cd6e81f67597f0c5de8a0af
Safiullah <safiullahnaeemahmed@gmail.com>
2025-11-30 19:45:28 +0500

    Safiullah

---
+++ lib/splash_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Image(
+        height: double.infinity,
+        fit: BoxFit.fitHeight,
+        image: NetworkImage(
+          'https://images.pexels.com/photos/247851/pexels-photo-247851.jpeg',
+        ),
+      ),
+    );
+  }
+}
